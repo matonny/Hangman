eb130529a069e8709bda3b9e1354447a0e2926cb
matonny <onny.mat@gmail.com>
2020-05-25 15:40:29 +0200

    initial files

---
+++ cmake-build-debug/CMakeFiles/Hangman.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hangman.dir/main.cpp.o"
+  "Hangman"
+  "Hangman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hangman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
